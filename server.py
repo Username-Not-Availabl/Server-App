cab1aca52d92884efcd9d6f177711302c75cb4cc
Username-Not-Availabl <adimchimma.odor001@albright.edu>
2022-12-13 12:29:16 -0500

    Revert "(1) Added basic server code"
    This reverts commit 266030f4a753352759c055ea72e4a57ae5d07cd5.

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 
-# test to see if changes are pushed
 # https://stackoverflow.com/questions/2816369/git-push-error-remote-rejected-master-master-branch-is-currently-checked
 
 from vidstream import AudioSender, AudioReceiver, ScreenShareClient, CameraClient, StreamingServer
